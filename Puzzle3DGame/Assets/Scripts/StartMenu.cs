178b379cac0a559e48a79b09b04b7e708de306fc
HoangKolya <82539362+HoangKolya@users.noreply.github.com>
2022-08-15 21:53:55 +0300

    Added menu and fixed some things
    Added menu

---
+++ Puzzle3DGame/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    Animator animator;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    public void Play()
+    {
+        animator.SetBool("isPlay", true);
+
+        Invoke("NextLvl", 0.5f);
+    }
+
+    public void Quit()
+    {
+        animator.SetBool("isQuit", true);
+        Invoke("Application.Quit", 0.5f);
+        
+    }
+
+    public void NextLvl()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
